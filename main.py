9db88dafb908248ee58b0d69d013082ab5d6747b
Daniela Gerena Lopera <danigerena.lopera@gmail.com>
2026-04-09 20:57:22 -0500

    Fix: Actualización main aumento parámetros de búsqueda

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
 import os
 
-# Asegurar que Python reconozca la carpeta src como un paquete
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# Importamos el trabajo de los 3 integrantes
-# NOTA: Al importar data, se ejecutará su bloque de prueba e imprimirá el tamaño de un lote. Es normal.
 from src.data import obtener_dataloaders
 from src.modelo import entrenar_modelo
 from src.optimizador import ejecutar_nas
@@ -16,18 +13,16 @@ def main():
     print("="*60)
     
     # ---------------------------------------------------------
-    # 1. OBTENER LOS DATOS (Persona 1)
+    # 1. OBTENER LOS DATOS
     # ---------------------------------------------------------
     print("\n[1] Extrayendo los DataLoaders...")
-    # Llamamos al generador de K-Fold de tu compañero
     generador_folds = obtener_dataloaders(batch_size=64, n_splits=5)
     
-    # Extraemos solo el primer fold (train y val) para evaluar rápido los cromosomas
     train_loader, val_loader = next(generador_folds)
     print("DataLoaders extraídos correctamente")
 
     # ---------------------------------------------------------
-    # 2. CREAR LA FUNCIÓN PUENTE (Tu trabajo - Persona 2)
+    # 2. CREAR LA FUNCIÓN PUENTE
     # ---------------------------------------------------------
     print("\n[2] Configurando la función de Fitness con tu CNN...")
     
@@ -37,26 +32,25 @@ def main():
         Recibe un cromosoma, entrena tu modelo real con los datos reales,
         y devuelve el accuracy.
         """
-        # Usamos 3 épocas por individuo para que la búsqueda inicial no tome horas
+
         return entrenar_modelo(
             cromosoma=cromosoma, 
             dataloader_train=train_loader, 
             dataloader_val=val_loader, 
-            epocas=3 
+            epocas=10
         )
 
     # ---------------------------------------------------------
-    # 3. EJECUTAR EL OPTIMIZADOR (Persona 3)
+    # 3. EJECUTAR EL OPTIMIZADOR
     # ---------------------------------------------------------
     print("\n[3] Iniciando la Búsqueda de Arquitectura Neuronal (NAS)...")
     
-    # Pasamos tu función 'fitness_real' al código de tu compañero
     resultados = ejecutar_nas(
-        tam_poblacion=4,      # 4 arquitecturas distintas por generación
-        num_generaciones=2,   # 2 generaciones (para una prueba rápida)
+        tam_poblacion=10,      # 10 arquitecturas distintas por generación
+        num_generaciones=5,   # 5 generaciones
         prob_mutacion=0.3,
         elitismo=1,
-        fitness_fn=fitness_real, # <--- ¡Aquí ocurre la integración!
+        fitness_fn=fitness_real,
         verbose=True
     )
 
